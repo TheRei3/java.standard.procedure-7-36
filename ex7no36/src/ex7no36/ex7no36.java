978e8d2803347b438081912fcaff08cdbd944cad
admin <admin@DESKTOP-COIHK7E>
2021-09-03 09:07:46 +0900

    ex7no36

---
+++ ex7no36/src/ex7no36/ex7no36.java
@@ -0,0 +1,11 @@
+package ex7no36;
+
+public class ex7no36 {  //  401p
+	Object iv = new Object() { void method() {} };
+	static Object cv = new Object() { void method() {} };
+	
+	void myMethod() {
+		Object lv = new Object() { void method() {} };
+	}
+
+}
